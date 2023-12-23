a262bed0da12106a02b60e5f9f8f4d50e2ee5729
tajerry <ion9266109890@yandex.ru>
2023-12-23 14:58:46 +0300

    clear

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import Routed from "./components/Routed";
-import {useDispatch} from "react-redux";
 function App() {
     return (
         <Routed/>
     );
 }
-export default App
+export default App;
+
 
